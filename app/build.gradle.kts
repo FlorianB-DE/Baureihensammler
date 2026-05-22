688732c7e202a4a8007e7062261ca96b4d22232b
Florian Becker <info@florianbecker.eu>
2026-05-22 02:20:51 +0200

    hotfix Flirt 3 crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "eu.florianbecker.baureihensammler"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.6"
+        versionCode = 10
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
