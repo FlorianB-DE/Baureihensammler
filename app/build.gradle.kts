7f2d25d4cf62847c153def9367e3577ab0a07608
Florian Becker <info@florianbecker.eu>
2026-08-10 16:53:09 +0200

    version bump and desiro fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "eu.florianbecker.baureihensammler"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.7"
+        versionCode = 11
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
